7ad14445f9e537d3a7ae09270f310778ad733580
shenjinxiang <sjx-sword@163.com>
2016-06-12 08:09:28 +0800

    添加Person的构造函数

--- day01/src/com/likestar/reflect/Person.java
+++ day01/src/com/likestar/reflect/Person.java
@@ -1,5 +1,7 @@
 package com.likestar.reflect;
 
+import java.util.UUID;
+
 public class Person {
 
 	private String id;
@@ -7,6 +9,24 @@ public class Person {
 	private int age;
 	private String address;
 	private String desc;
+	
+	public Person() {
+		this.id = UUID.randomUUID().toString().replace("-", "");
+	}
+	
+	public Person(String name, int age) {
+		this.id = UUID.randomUUID().toString().replace("-", "");
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Person(String name, int age, String address, String desc) {
+		this.id = UUID.randomUUID().toString().replace("-", "");
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.desc = desc;
+	}
 
 	public String getId() {
 		return id;
