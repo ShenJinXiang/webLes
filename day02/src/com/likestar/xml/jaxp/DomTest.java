0dede06959860d1944a24816738ed0433bfa727f
shenjinxiang <sjx-sword@163.com>
2016-06-13 16:10:48 +0800

    自己的练习

---
+++ day02/src/com/likestar/xml/jaxp/DomTest.java
@@ -0,0 +1,47 @@
+package com.likestar.xml.jaxp;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class DomTest {
+
+	@Test
+	public void test1() throws ParserConfigurationException, FileNotFoundException, SAXException, IOException{
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+		File file = new File("src/xml", "books.xml");
+		Document document = documentBuilder.parse(new FileInputStream(file));
+		Node rootNode = document.getElementsByTagName("books").item(0);
+		NodeList bookNodes = rootNode.getChildNodes();
+		for(int i = 0; i < bookNodes.getLength(); i++) {
+			System.out.println("********************************");
+			Node bookNode = bookNodes.item(i);
+			NodeList nodes = bookNode.getChildNodes();
+			for(int j = 0; j < nodes.getLength(); j++) {
+				System.out.println("------------------------------");
+				Node nodeJ = nodes.item(j);
+				System.out.println("NodeName -> " + nodeJ.getNodeName());
+				System.out.println("NodeValue -> " + nodeJ.getNodeValue());
+				System.out.println("LocalName -> " + nodeJ.getLocalName());
+				System.out.println("TextContent --> " + nodeJ.getTextContent());
+				System.out.println("NodeType -> " + nodeJ.getNodeType());
+				System.out.println("------------------------------");
+			}
+			System.out.println("********************************");
+		}
+		
+	}
+}
