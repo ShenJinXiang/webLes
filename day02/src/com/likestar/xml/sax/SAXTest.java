1c00297564179e5855271ae6cf75873138153c2a
shenjinxiang <sjx-sword@163.com>
2016-06-14 09:58:50 +0800

    sax 第一步测试

--- day02/src/com/likestar/xml/sax/SAXTest.java
+++ day02/src/com/likestar/xml/sax/SAXTest.java
@@ -1,11 +1,51 @@
 package com.likestar.xml.sax;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.junit.Test;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXTest {
 
 	@Test
-	public void test1() {
+	public void test1() throws Exception {
+		// 创建解析工厂
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		
+		// 创建解析器
+		SAXParser parser = factory.newSAXParser();
+		
+		// 读取器
+		XMLReader reader = parser.getXMLReader();
+		
+		reader.setContentHandler(new ListHandler());
+		
+		// 读取xml内容
+		reader.parse("src/xml/books.xml");
+	}
+	
+	class ListHandler extends DefaultHandler {
+
+		@Override
+		public void startElement(String uri, String localName, String qName, Attributes attributes)
+				throws SAXException {
+			System.out.println("startElement : uri -> " + uri + "\tlocalName -> " + localName + "\tqName -> " + qName);
+		}
+
+		@Override
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			System.out.println("endElement : uri -> " + uri + "\tlocalName -> " + localName + "\tqName -> " + qName);
+		}
+
+		@Override
+		public void characters(char[] ch, int start, int length) throws SAXException {
+			System.out.println("characters");
+		}
 		
 	}
 }
