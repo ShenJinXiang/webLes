9a9cd4f1c199f976d1ace88e9ae5ec9dc33872ff
ShenJinXiang <likestarsjx@gmail.com>
2016-06-18 10:32:11 +0800

    day04

---
+++ day04/src/com/shenjinxiang/servlet/ServletDemo.java
@@ -0,0 +1,27 @@
+package com.shenjinxiang.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletDemo extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		test1(req, resp);
+	}
+	
+	public void test1(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String str = "<h1>I LOVE fangfang!!!</h1>";
+		resp.getWriter().write(str);
+	}
+
+}
