1b8cf8f36ebe41d1964a4f9a17150d494ad4cb82
shenjinxiang <sjx-sword@163.com>
2016-06-19 23:04:50 +0800

    servlet

--- day04/src/com/shenjinxiang/servlet/ServletDemo.java
+++ day04/src/com/shenjinxiang/servlet/ServletDemo.java
@@ -16,12 +16,18 @@ public class ServletDemo extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		test1(req, resp);
+		test2(req, resp);
 	}
 	
 	public void test1(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String str = "<h1>I LOVE fangfang!!!</h1>";
+		resp.setHeader("Content-Type", "text/html");
 		resp.getWriter().write(str);
+	}
+	
+	public void test2(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String str = "Hello World";
+		resp.getOutputStream().write(str.getBytes());
 	}
 
 }
