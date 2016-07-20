13f97c61c0f6dd4d7b34002d262d2d2030a0f88f
shenjinxiang <sjx-sword@163.com>
2016-07-20 21:23:27 +0800

    update

--- day19/src/com/shenjinxiang/filter/NoCacheFilter.java
+++ day19/src/com/shenjinxiang/filter/NoCacheFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class FilterDemo2 implements Filter {
+public class NoCacheFilter implements Filter {
 
 	@Override
 	public void destroy() {
