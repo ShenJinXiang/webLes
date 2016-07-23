b917e5f3a205fb07dbbeba95abae1f91c4b76420
shenjinxiang <sjx-sword@163.com>
2016-07-24 00:13:03 +0800

    timer demo 1

--- day21/src/com/shenjinxiang/demo/Demo4.java
+++ day21/src/com/shenjinxiang/demo/Demo4.java
@@ -11,18 +11,11 @@ public class Demo4 {
 	public static void main(String[] args) {
 		Timer timer = new Timer();
 		timer.schedule(new DemoTask(), 0, 1000);
-//		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
-//		Calendar c = Calendar.getInstance();
-//		c.set(2016, 6, 23, 23, 35, 0);
-//		Date date = c.getTime();
-//		System.out.println(sdf.format(date));
-//		timer.schedule(new DemoTask(), c.getTime());
 	}
 }
 
 class DemoTask extends TimerTask {
 
-	
 	@Override
 	public void run() {
 		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
