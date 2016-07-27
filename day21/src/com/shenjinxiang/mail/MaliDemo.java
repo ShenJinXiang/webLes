c52bcb94d05ff37ae1e6f56c5840c47ec9562217
shenjinxiang <sjx-sword@163.com>
2016-07-27 13:32:00 +0800

    sdfds

---
+++ day21/src/com/shenjinxiang/mail/MaliDemo.java
@@ -0,0 +1,18 @@
+package com.shenjinxiang.mail;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.junit.Test;
+
+public class MaliDemo {
+
+	@Test
+	public void test() throws UnknownHostException, IOException {
+	}
+}
