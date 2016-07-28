a3a11e47e121ed11a0fdb68c6766fc147e3cbf94
shenjinxiang <sjx-sword@163.com>
2016-07-28 15:22:32 +0800

    dfds

--- parse/src/com/shenjinxiang/parse/main/ParseHTML.java
+++ parse/src/com/shenjinxiang/parse/main/ParseHTML.java
@@ -5,10 +5,10 @@ import com.shenjinxiang.parse.utils.Md2Html;
 public class ParseHTML {
 
 	public static void main(String[] args) {
-		if(args.length != 1) {
-			System.out.println("参数错误");
-		}
-		Md2Html.asHtml(args[0]);
-//		Md2Html.asHtml("d:\\test.md");
+//		if(args.length != 1) {
+//			System.out.println("参数错误");
+//		}
+//		Md2Html.asHtml(args[0]);
+		Md2Html.asHtml("d:\\test.md");
 	}
 }
--- parse/src/com/shenjinxiang/parse/utils/Md2Html.java
+++ parse/src/com/shenjinxiang/parse/utils/Md2Html.java
@@ -1,12 +1,10 @@
 package com.shenjinxiang.parse.utils;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
@@ -40,7 +38,7 @@ public class Md2Html {
 			List<String> htmlList = getHtml(mdList);
 			for(String html : htmlList) {
 				os.write(html);
-				
+				System.out.println(html);
 			}
 			os.flush();
 		} catch (FileNotFoundException e) {
