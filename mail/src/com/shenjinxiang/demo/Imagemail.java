0166f28dd6fb0838beff6da54791a5f79b4e3225
shenjinxiang <sjx-sword@163.com>
2016-07-28 09:08:05 +0800

    dsdfsd

--- mail/src/com/shenjinxiang/demo/Imagemail.java
+++ mail/src/com/shenjinxiang/demo/Imagemail.java
@@ -1,7 +1,12 @@
 package com.shenjinxiang.demo;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -9,10 +14,11 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class Imagemail {
 
-	public static void main(String[] args) throws AddressException, MessagingException {
+	public static void main(String[] args) throws AddressException, MessagingException, FileNotFoundException, IOException {
 		// 创建邮件
 		Session session = Session.getDefaultInstance(new Properties());
 		MimeMessage message = new MimeMessage(session);
@@ -26,7 +32,21 @@ public class Imagemail {
 		MimeBodyPart text = new MimeBodyPart();
 		text.setContent("aaaaaaa<br><img src='cid:1.jgp'><br>aaaaa", "text/html");
 		
+		// 创建图片
+		MimeBodyPart image = new MimeBodyPart();
+		DataHandler dh = new DataHandler(new FileDataSource("src/1.jpg")); // jaf
+		image.setDataHandler(dh);
+		image.setContentID("1.jpg");
 		
+		// 描述数据之间的关系
+		MimeMultipart mm = new MimeMultipart();
+		mm.addBodyPart(text);
+		mm.addBodyPart(image);
+		mm.setSubType("related"); // mixed
 		
+		message.setContent(mm);
+		message.saveChanges();
+		
+		message.writeTo(new FileOutputStream("d:\\1.eml"));
 	}
 }
