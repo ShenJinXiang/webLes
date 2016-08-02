45422bd33a76d38fe862f2b2d8f8aa8e178b9d45
shenjinxiang <sjx-sword@163.com>
2016-08-02 22:00:44 +0800

    base 64

--- mail/src/com/shenjinxiang/demo/Demo1.java
+++ mail/src/com/shenjinxiang/demo/Demo1.java
@@ -2,6 +2,8 @@ package com.shenjinxiang.demo;
 
 import java.io.IOException;
 
+import org.apache.commons.codec.binary.Base64;
+
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
@@ -13,8 +15,11 @@ public class Demo1 {
 	
 	public static void main(String[] args) {
 		jdkBase64(src);
+		commonsCodesBase64(src);
+		bouncyCastleBase64(src);
 	}
 	
+	// jdk
 	public static void jdkBase64(String str) {
 		try {
 			BASE64Encoder encoder = new BASE64Encoder();
@@ -27,5 +32,24 @@ public class Demo1 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	// commons codec
+	public static void commonsCodesBase64(String src) {
+		byte[] encodeBytes = Base64.encodeBase64(src.getBytes());
+		System.out.println(new String(encodeBytes));
+		
+		byte[] decodeBytes = Base64.decodeBase64(encodeBytes);
+		System.out.println(new String(decodeBytes));
+	}
+	
+	// bouncy castle
+	public static void bouncyCastleBase64(String src) {
+		byte[] encodeBytes = org.bouncycastle.util.encoders.Base64.encode(src.getBytes());
+		System.out.println(new String(encodeBytes));
+		
+		byte[] decodeBytes = org.bouncycastle.util.encoders.Base64.decode(encodeBytes);
+		System.out.println(new String(decodeBytes));
+		
 	}
 }
